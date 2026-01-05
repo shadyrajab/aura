0a001e40847251230306066d359cdd303275ed0f
Shady Rajab <shadyrajaab@gmail.com>
2026-01-05 11:31:28 -0300

    style: change message

--- src/commands/topaura.ts
+++ src/commands/topaura.ts
@@ -1,7 +1,6 @@
 import { ChatInputCommandInteraction, EmbedBuilder } from "discord.js";
 import { DataSource } from "typeorm";
 import { User } from "../models/user";
-import { formatRelativeTime } from "../utils/dateUtils";
 
 export const AuraRankingData = {
   name: "topaura",
@@ -34,8 +33,7 @@ export const AuraRankingExecute = async (
       const username = discordUser
         ? discordUser.username
         : `Usuário desconhecido (${user.discordId})`;
-      const lastPost = formatRelativeTime(user.updatedAt);
-      return `**${index + 1}º** - ${username}: **${user.aura}** aura (última postagem: ${lastPost})`;
+      return `**${index + 1}º** - ${username}: **${user.aura}** aura`;
     }),
   );
 
